71e19efbe035d9f925823c43c0203aa35000336f
Sam <sambhavvinayak16@gmail.com>
2025-08-23 22:58:25 +0530

    Configure static and media file settings
    Updated Django settings to set STATIC_URL to '/static/', added STATICFILES_DIRS for project-level and custom static folders, and defined STATIC_ROOT for collected static files. Also added MEDIA_URL and MEDIA_ROOT for user-uploaded media. Added collected static files for the admin and htmx to the staticfiles directory.

--- CodePlatform/settings.py
+++ CodePlatform/settings.py
@@ -121,10 +121,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static",                # project-level static
+    BASE_DIR / "assets",                # extra custom folder
 ]
+
+# Where collected static files will be stored (for production)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media files (user uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Default primary key field type
