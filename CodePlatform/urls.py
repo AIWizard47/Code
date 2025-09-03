d5fc3339333b8f5122008060ebd7036024ab727f
Sam <sambhavvinayak16@gmail.com>
2025-09-03 23:42:54 +0530

    Change Django admin URL path to /sam/admin/
    Updated the Django admin interface URL from '/admin/' to '/sam/admin/' in urls.py. This may be for security through obscurity or to avoid conflicts with other routes.

--- CodePlatform/urls.py
+++ CodePlatform/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('sam/admin/', admin.site.urls),
     path('administrator/',include('administrator.urls')),
     path("",include("base.urls")),
     path("problems/",include("problems.urls")),
